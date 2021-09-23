da71c00d2e8957a5599038700e16f49b38dd632a
Christian Oder <myself5@carbonrom.org>
2021-09-23 15:34:22 +0700

    juice: BoardConfig: Build disabled VBMeta image
    Signed-off-by: Inok <inok.dr189@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,5 +165,10 @@ BOARD_VNDK_VERSION := current
 ENABLE_VIRTUAL_AB := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 # Inherit from the proprietary version
 include vendor/xiaomi/juice/BoardConfigVendor.mk
