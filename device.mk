7604e3f493ba71a1d7ada33f44f1c0318fa6434b
Inok <inok.dr189@gmail.com>
2021-09-23 22:55:42 +0700

    juice: Import sensor configs
    Import From qssi-user-11-RKQ1.201004.002-21.9.15-release-keys
    
    Signed-off-by: Inok <inok.dr189@gmail.com>

--- device.mk
+++ device.mk
@@ -101,6 +101,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Sensor
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
