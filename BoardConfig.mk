38334b2e04745105e0984c6ec8e7009d74aa3519
Inok <inok.dr189@gmail.com>
2021-09-23 12:20:04 +0700

    juice: Import proprietary files and proprietary build
    Import From qssi-user-11-RKQ1.201004.002-21.9.15-release-keys
    
    Signed-off-by: Inok <inok.dr189@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,5 +51,10 @@ TARGET_OTA_ASSERT_DEVICE := lime,citrus,lemon,pomelo,juice
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit-juice
 TARGET_RECOVERY_DEVICE_MODULES := libinit-juice
 
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit from the proprietary version
 include vendor/xiaomi/juice/BoardConfigVendor.mk
