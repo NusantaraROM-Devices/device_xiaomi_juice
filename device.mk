2958ae4e3b6e412505c9d9382802c13d41e698aa
Inok <inok.dr189@gmail.com>
2021-09-25 22:19:46 +0700

    juice: Build required display packages
    * Build vendor display config
    * Build HW memtrack
    * Build HW graphics composer
    * Build HW graphics allocator
    * Build HW graphics mapper
    * Build vendor qti display composer
    * Build vendor qti display allocator
    * Build vendor qti display mapper
    * Build lib display
    
    Signed-off-by: Inok <inok.dr189@gmail.com>

--- device.mk
+++ device.mk
@@ -164,6 +164,60 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += GoogleCameraGo
 
 # Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1 \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.2 \
+    android.hardware.graphics.composer@2.2-impl \
+    android.hardware.graphics.composer@2.3 \
+    android.hardware.graphics.composer@2.3-impl \
+    android.hardware.graphics.composer@2.4 \
+    android.hardware.graphics.composer@2.4-impl \
+    android.hardware.graphics.allocator@2.0 \
+    android.hardware.graphics.allocator@3.0 \
+    android.hardware.graphics.allocator@4.0 \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
+
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.15 \
+    vendor.display.config@1.15.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.composer@1.0.vendor \
+    vendor.qti.hardware.display.composer@2.0.vendor \
+    vendor.qti.hardware.display.composer@3.0 \
+    vendor.qti.hardware.display.composer@3.0.vendor \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.allocator@1.0.vendor \
+    vendor.qti.hardware.display.allocator@3.0.vendor \
+    vendor.qti.hardware.display.allocator@4.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.capabilityconfigstore@1.0-service \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    memtrack.bengal \
+    gralloc.bengal \
+    hwcomposer.bengal \
+
+PRODUCT_PACKAGES += \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libqdMetaData \
+    libsdmcore \
+    libsdmutils \
+    libtinyxml
+
+PRODUCT_PACKAGES += \
+    libvulkan
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
