e5fbd7689ccf73c5c1f28816984f509314ac28b4
I-n-o-k <inok.dr189@gmail.com>
2022-01-12 11:07:09 +0700

    juice: Build required fingerprint packages
    Signed-off-by: I-n-o-k <inok.dr189@gmail.com>

--- device.mk
+++ device.mk
@@ -275,7 +275,13 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1
+
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Cas
 PRODUCT_PACKAGES += \
