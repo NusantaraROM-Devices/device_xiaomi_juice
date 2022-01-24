4c0994b0696d19438a1bbae914777197df915e9f
ganomin <szymon.wirkus12@gmail.com>
2022-01-24 19:31:37 +0100

    juice: Import NFC configs
    * from lime-user-11-RKQ1.201004.002-21.11.3-release-keys
    
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>
    Change-Id: Ie674ba0cd61184603bc087ed9e38b5155caa3bcc

--- device.mk
+++ device.mk
@@ -132,6 +132,11 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2 \
     android.hardware.neuralnetworks@1.2.vendor
 
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/libnfc-hal-st.conf
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
