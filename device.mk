3116794ac37857c3a30ea3eb4056c5f860ff42b5
frost (frostg-012) <optimus7907@gmail.com>
2022-02-26 09:12:13 +0700

    Revert "juice: Add Pixel 2016 exclusive xml"
    This reverts commit 3b3334ec1163493fdd33a76355fe0a5432dfd1a1.
    
    Change-Id: I8ee360a04918b13834bd06c6ae8e4bd80772a473

--- device.mk
+++ device.mk
@@ -76,10 +76,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0 \
     android.hardware.gatekeeper@1.0.vendor
 
-# Google Photos
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/pixel_2016_exclusive.xml
-
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1 \
