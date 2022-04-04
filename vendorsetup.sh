6a63562fe08911fdb753f12f8610e5cf499a827b
I-n-o-k <inok.dr189@gmail.com>
2022-04-04 14:15:18 +0700

    juice: Add vendorsetup.sh
    Signed-off-by: I-n-o-k <inok.dr189@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf vendor/nxp/opensource/commonsys/packages/apps/Nfc
+git clone https://github.com/I-n-o-k/android_vendor_nxp_opensource_packages_apps_Nfc vendor/nxp/opensource/commonsys/packages/apps/Nfc
